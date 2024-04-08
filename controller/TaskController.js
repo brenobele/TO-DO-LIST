d596fa3a0a3ddb4d1bea5d9472a1f484efd9749f
Breno S. Beletato <breno.beletato@suprema.group>
2024-04-07 23:24:23 -0300

    Update TaskController to handle getById, updateOneTask, and deleteOneTask

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -3,7 +3,7 @@ const Task = require("../models/Task");
 const getAllTasks = async (req, res) => {
   try {
     const taskList = await Task.find().sort({ date: "desc" });
-    return res.render("index", { taskList });
+    return res.render("index", { task: null, taskList, taskDelete: null });
   } catch (err) {
     return res.status(500).send({ error: err.message });
   }
@@ -20,7 +20,41 @@ const createTask = async (req, res) => {
     await Task.create(task);
     return res.redirect("/");
   } catch (err) {
-    console.log({ err });
+    return res.status(500).send({ error: err.message });
+  }
+};
+
+const getById = async (req, res) => {
+  try {
+    const taskList = await Task.find().sort({ date: "desc" });
+    if (req.params.method == "update") {
+      const task = await Task.findOne({ _id: req.params.id });
+      res.render("index", { task, taskDelete: null, taskList });
+    } else {
+      const taskDelete = await Task.findOne({ _id: req.params.id });
+      res.render("index", { task: null, taskDelete, taskList });
+    }
+  } catch (err) {
+    return res.status(500).send({ errorgetById: err.message });
+  }
+};
+
+const updateOneTask = async (req, res) => {
+  try {
+    const task = req.body;
+
+    await Task.updateOne({ _id: req.params.id }, task);
+    res.redirect("/");
+  } catch (err) {
+    return res.status(500).send({ error: err.message });
+  }
+};
+
+const deleteOneTask = async (req, res) => {
+  try {
+    await Task.deleteOne({ _id: req.params.id });
+    res.redirect("/");
+  } catch (err) {
     return res.status(500).send({ error: err.message });
   }
 };
@@ -28,4 +62,7 @@ const createTask = async (req, res) => {
 module.exports = {
   getAllTasks,
   createTask,
+  getById,
+  updateOneTask,
+  deleteOneTask,
 };

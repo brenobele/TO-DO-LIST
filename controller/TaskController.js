95ea510fec7b84fd28d9afc30c03fbd607b79986
Breno S. Beletato <breno.beletato@suprema.group>
2024-04-10 23:15:13 -0300

    Update TaskController to handle displaying messages in the index view

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -1,9 +1,21 @@
 const Task = require("../models/Task");
 
+let message = "";
+let type = "";
+
 const getAllTasks = async (req, res) => {
   try {
+    setTimeout(() => {
+      message = "";
+    }, 1000);
     const taskList = await Task.find().sort({ date: "desc" });
-    return res.render("index", { task: null, taskList, taskDelete: null });
+    return res.render("index", {
+      task: null,
+      taskList,
+      taskDelete: null,
+      message,
+      type
+    });
   } catch (err) {
     return res.status(500).send({ error: err.message });
   }
@@ -13,13 +25,19 @@ const createTask = async (req, res) => {
   const task = req.body;
 
   if (!task.task) {
+    message = "Insira um texto antes de adicionar a tarefa!";
+    type = "danger";
     return res.redirect("/");
   }
 
   try {
     await Task.create(task);
+    message = "Tarefa criada com sucesso!";
+    type = "success";
     return res.redirect("/");
   } catch (err) {
+    message = "Erro não esperado <i class=\"bi bi-emoji-frown-fill\"></i>";
+    type = "warning";
     return res.status(500).send({ error: err.message });
   }
 };
@@ -29,12 +47,14 @@ const getById = async (req, res) => {
     const taskList = await Task.find().sort({ date: "desc" });
     if (req.params.method == "update") {
       const task = await Task.findOne({ _id: req.params.id });
-      res.render("index", { task, taskDelete: null, taskList });
+      res.render("index", { task, taskDelete: null, taskList, message, type });
     } else {
       const taskDelete = await Task.findOne({ _id: req.params.id });
-      res.render("index", { task: null, taskDelete, taskList });
+      res.render("index", { task: null, taskDelete, taskList, message, type });
     }
   } catch (err) {
+    message = 'Erro não esperado <i class="bi bi-emoji-frown-fill"></i>';
+    type = "warning";
     return res.status(500).send({ errorgetById: err.message });
   }
 };
@@ -44,8 +64,12 @@ const updateOneTask = async (req, res) => {
     const task = req.body;
 
     await Task.updateOne({ _id: req.params.id }, task);
+    message = "Tarefa atualizada com sucesso!";
+    type = "success";
     res.redirect("/");
   } catch (err) {
+    message = 'Erro não esperado <i class="bi bi-emoji-frown-fill"></i>';
+    type = "warning";
     return res.status(500).send({ error: err.message });
   }
 };
@@ -53,8 +77,12 @@ const updateOneTask = async (req, res) => {
 const deleteOneTask = async (req, res) => {
   try {
     await Task.deleteOne({ _id: req.params.id });
+    message = "Tarefa apagada com sucesso!";
+    type = "success";
     res.redirect("/");
   } catch (err) {
+    message = 'Erro não esperado <i class="bi bi-emoji-frown-fill"></i>';
+    type = "warning";
     return res.status(500).send({ error: err.message });
   }
 };
